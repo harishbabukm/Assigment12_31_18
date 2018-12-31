a041ad261e41bdbc7b70e74919e0f3a822078449
harishbabukm <43536467+harishbabukm@users.noreply.github.com>
2018-12-31 15:55:22 +0530

    Update Harishbabu.java

--- First/src/typattern/Harishbabu.java
+++ First/src/typattern/Harishbabu.java
@@ -5,7 +5,7 @@ public class Harishbabu {
 	public static void main(String[] args) {
 		System.out.println("hi harishbabu welcome back");
 		System.out.println("welcome to test yantra");
-		System.out.println("hi harishbabu welcome back");
+		
 	}
 
 }
